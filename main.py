e1b9bf21ff0d4bd7e9efde2b0243b647aa2842e5
LyubovGuzhvina <guzhvinalub@gmail.com>
2024-03-24 22:29:02 +0300

    some changes

--- main.py
+++ main.py
@@ -26,10 +26,16 @@ from redis import asyncio as aioredis
 
 app = FastAPI()
 
+redis_url = 'rediss://red-co06gvi0si5c73fi6r80:LXRfrjxT6TEX3WE0J8cxsuUiMe1BJbex@frankfurt-redis.render.com:6379'
+redis = redis.Redis.from_url(redis_url)
+
 @app.on_event("startup")
 async def startup_event():
-    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+def cache_key_generation(text):
+    return hashlib.md5(str(text).encode('utf-8')).hexdigest()
+
 
 @app.get("/hello_world")
 @cache(expire=60)
@@ -37,9 +43,7 @@ async def index():
     return dict(hello = "world")
 
 class Item(BaseModel):
-    name: str
     year: int
-    selling_price: int
     km_driven: int
     fuel: str
     seller_type: str
@@ -65,15 +69,46 @@ def get_data():
     return FileResponse(path = 'cars_train.csv', filename='cars_train.csv')
 
 @app.get("/get_test_data")
-@cache(expire=60)
 #download original dataset (test)
 def get_data():
     data_test = urllib.request.urlretrieve('https://raw.githubusercontent.com/hse-mlds/ml/main/hometasks/HT1/cars_test.csv', 'cars_test.csv')
-    return FileResponse(path = 'cars_test.csv', filename='cars_test.csv'),
+    return FileResponse(path = 'cars_test.csv', filename='cars_test.csv')
 
 @app.post("/predict_item")
 #obtain a model prediction for single user item
-@cache(expire=60)
+def predict_item(item: Item) -> float:
+
+    cache_key = cache_key_generation(item)
+
+    if redis.exists(cache_key):
+        prediction = pickle.loads(redis.get(cache_key))
+        return {"prediction": prediction, 'From cash': True}
+    else:
+        input_data = item.json()
+        input_dictionary = json.loads(input_data)
+        year = input_dictionary["year"]
+        km_driven = input_dictionary["km_driven"]
+        fuel = input_dictionary["fuel"]
+        seller_type = input_dictionary["seller_type"]
+        transmission = input_dictionary["transmission"]
+        owner = input_dictionary["owner"]
+        mileage = input_dictionary["mileage"]
+        engine = input_dictionary["engine"]
+        max_power = input_dictionary["max_power"]
+        seats = input_dictionary["seats"]
+        my_list = [year, km_driven, fuel, seller_type, transmission, owner, mileage, engine, max_power, seats]
+        my_list.append(max_power / engine)
+        my_list.append(year ** 2)
+        my_list = pd.DataFrame([my_list], columns=["year", "km_driven", "fuel", "seller_type", "transmission",
+                                                   "owner", "mileage", "engine", "max_power", "seats",
+                                                   "max_power_engine", "year_squared"])
+        tramsformed_list = transformer.transform(my_list)
+        scaled_list = scaler.transform(tramsformed_list)
+        prediction = cars_model.predict(scaled_list)
+        redis.set(cache_key, pickle.dumps(prediction))
+        return {"prediction": prediction, 'From cash': False}
+
+@app.post("/predict_item1")
 def predict_item(item: Item) -> float:
     input_data = item.json()
     input_dictionary = json.loads(input_data)
@@ -96,8 +131,14 @@ def predict_item(item: Item) -> float:
                                                "owner", "mileage", "engine", "max_power", "seats", "max_power_engine", "year_squared"])
     tramsformed_list = transformer.transform(my_list)
     scaled_list = scaler.transform(tramsformed_list)
-    prediction =  cars_model.predict(scaled_list)
+    prediction = cars_model.predict(scaled_list)
     return prediction
+
+
+
+
+
+
 
 
 @app.post("/uploadcsv/")
@@ -110,8 +151,9 @@ def upload_csv(csv_file: UploadFile = File(...)) -> List[float]:
     return df["selling_price_pred"]
 
 @app.get("/ping")
-@cache(expire=60)
 async def ping():
     return {"message": "pong"}
+
+
 
 
